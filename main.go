320b6489f9f6d5b2c6bf6f292643d36b241dec0a
Pietro Carrara <piticarrara@gmail.com>
2018-11-15 02:31:38 -0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+func main() {
+	address, _ := url.Parse("http://api2.musical.ly/")
+	proxy := httputil.NewSingleHostReverseProxy(address)
+
+	proxy.Director = func(r *http.Request) {
+
+		r.Header.Set("User-Agent", "com.zhiliaoapp.musically/2018110931")
+
+		r.URL.Scheme = address.Scheme
+		r.URL.Host = address.Host
+
+		r.Host = address.Host
+
+		fmt.Println(r)
+	}
+
+	proxy.ModifyResponse = func(r *http.Response) error {
+
+		r.Header.Set("Access-Control-Allow-Origin", "*")
+		return nil
+	}
+
+	go http.ListenAndServe(":8181", proxy)
+
+	static := http.FileServer(http.Dir("./static/"))
+	http.Handle("/", static)
+
+	http.Handle("/Exit", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		os.Exit(0)
+	}))
+
+	http.ListenAndServe(":8080", nil)
+}
---
+++ static/templates.js
@@ -0,0 +1,15 @@
+var templates = {}
+
+templates.parse = function(tplName, args) {
+
+	return fetch(`/templates/${tplName}`)
+	.then(res => res.text())
+	.then(tpl => {
+		for (__i in args) {
+			eval(`var ${__i} = args[__i]`)
+		}
+
+		return eval('`' + tpl + '`')
+	})
+
+}
