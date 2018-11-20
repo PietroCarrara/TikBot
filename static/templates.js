087c8705d658188bb2923d64c2b3b67ed9bc1c9f
Pietro Carrara <piticarrara@gmail.com>
2018-11-19 23:36:17 -0200

    Update aweme template

--- static/templates.js
+++ static/templates.js
@@ -7,7 +7,6 @@ templates.parse = function(tplName, args) {
 	.then(tpl => {
 		for (__i in args) {
 			eval(`var ${__i} = args[__i]`)
-			console.log(`var ${__i} = args[__i]`)
 		}
 
 		return eval('`' + tpl + '`')
